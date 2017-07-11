60f97f8cc7b651db2e4d46c1691a0d9853b4cd10
vivek ghanchi <vivekghanchi12345@gmail.com>
2017-07-11 11:39:25 +0530

    Robin_Hood VR Game
    This is the Virtual Reality game which is developed using Unity and SteamVR plaugins that will  run basically on HTC Vive

---
+++ Assets/scripts/Arrow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+	private bool isAttached = false;
+	private bool isFired =false;
+
+	void OnTriggerStay(){
+		AttachArrow ();
+	}
+
+	void OnTriggerEnter(){
+		AttachArrow ();
+	}
+
+	void Update (){
+		if (isFired) {
+			transform.LookAt (transform.position + transform.GetComponent<Rigidbody> ().velocity);
+		}
+	}
+
+	public void Fired(){
+		isFired = true;
+	}
+
+	private void AttachArrow(){
+
+		var device = SteamVR_Controller.Input((int)arrowManager.instance.trackedObj.index);
+		if (!isAttached && device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+			arrowManager.instance.AttachBowToArrow ();
+			isAttached = true;
+		}
+	}
+		
+}
---
+++ Assets/scripts/arrowManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowManager : MonoBehaviour {
+
+	public static arrowManager instance;
+
+	public SteamVR_TrackedObject trackedObj;
+
+	private GameObject currentArrow;
+	public GameObject arrowPrefab;
+	public GameObject stringAttachPoint;
+	public GameObject arrowStartPoint;
+	public GameObject stringStartPoint;
+
+	private bool isAttach = false;
+
+	void Awake(){
+		if (instance == null)
+			instance = this;
+	}
+
+	void onDestroy(){
+		if (instance == this)
+			instance = null;
+	}
+
+	void Start() {
+		
+	}
+
+	void Update() {
+		attachArrow ();
+		PullString ();
+	}
+
+	private void PullString(){
+		if (isAttach) {
+			float dist = (stringStartPoint.transform.position - trackedObj.transform.position).magnitude;
+			stringAttachPoint.transform.position = stringStartPoint.transform.localPosition + new Vector3 (5F * dist, 0f, 0f);
+
+			var device = SteamVR_Controller.Input((int)trackedObj.index);
+			if (device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger)) {
+				fire ();
+			}
+		}
+	}
+
+	private void fire(){
+
+		float dist = (stringStartPoint.transform.position - trackedObj.transform.position).magnitude;
+
+		currentArrow.transform.parent = null;
+		currentArrow.GetComponent<Arrow> ().Fired ();
+		Rigidbody r = currentArrow.GetComponent<Rigidbody> ();
+		r.velocity = currentArrow.transform.forward * 10f* dist;
+		r.useGravity = true;
+
+		currentArrow.GetComponent<Collider> ().isTrigger = false;
+
+		stringAttachPoint.transform.position = stringStartPoint.transform.position;
+		currentArrow = null;
+		isAttach = false;
+	}
+
+	private void attachArrow(){
+		if(currentArrow == null){
+			currentArrow = Instantiate (arrowPrefab);
+			currentArrow.transform.parent = trackedObj.transform;
+			currentArrow.transform.localPosition = new Vector3 (0f, 0f, .342f);
+			currentArrow.transform.localRotation = Quaternion.identity;
+		}
+	}
+
+	public void AttachBowToArrow(){
+		currentArrow.transform.parent = stringAttachPoint.transform;
+		currentArrow.transform.localPosition = arrowStartPoint.transform.localPosition;
+		currentArrow.transform.rotation = arrowStartPoint.transform.rotation;
+
+		isAttach = true;
+	}
+}
